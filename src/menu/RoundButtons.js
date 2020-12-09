5389cff195fbd3e0058c4d71cd0646e44b8213dc
GoncharovCyril <goncharov@donticket.ru>
2020-12-10 00:56:31 +0300

    Add linking to socnets in Menu. Instead telegram link go to fb

--- src/menu/RoundButtons.js
+++ src/menu/RoundButtons.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Linking } from 'react-native';
 
 import VkBrandsRound from '../../svg/rounds/vk-brands-round';
 import TelegramPlaneBrandsRound from '../../svg/rounds/telegram-plane-brands-round';
@@ -7,6 +7,7 @@ import OdnoklassnikiBrandsRound from '../../svg/rounds/odnoklassniki-brands-roun
 import InstagramBrandsRound from '../../svg/rounds/instagram-brands-round';
 
 import { BoxShadow } from 'react-native-shadow';
+import { Link } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
 
@@ -28,33 +29,53 @@ const shadowOpt = {
     x: 0,
     y: 0,
     style: {}
+};
+
+
+const openSite = (url) => {
+    return React.useCallback(async () => {
+        const supported = await Linking.canOpenURL(url);
+    
+        if (supported) {
+            await Linking.openURL(url);
+        }
+        else {
+            alert("Don't know how to open this URL");
+        }
+    }, [url]);
 }
 
 const RoundButtons = ({ navigation }) => {
+    
+    const openVk = openSite("https://vk.com/club172375146");
+    const openTelegram = openSite("https://www.facebook.com/groups/696310800761799/");
+    const openOk = openSite("https://ok.ru/group/54231360864470");
+    const openInstagram = openSite("https://www.instagram.com/aptekanarodnaya/");
+
     return (
         <View style={styles.roundButtonsContainer}>
             <View style={{ width: 10 }} />
             <BoxShadow setting={shadowOpt} >
-                <TouchableOpacity style={{ height: buttonSize, width: buttonSize }} >
+                <TouchableOpacity style={{ height: buttonSize, width: buttonSize }} onPress={openVk}>
                     <VkBrandsRound color="white" roundColor="rgb(226,94,18)"/>
                 </ TouchableOpacity>
             </BoxShadow>
 
             <View style={{ width: 10 }} />
             <BoxShadow setting={shadowOpt} >
-                <TouchableOpacity style={{ height: buttonSize, width: buttonSize }} >
+                <TouchableOpacity style={{ height: buttonSize, width: buttonSize }} onPress={openTelegram}>
                     <TelegramPlaneBrandsRound color="white" roundColor="rgb(226,94,18)"/>
                 </ TouchableOpacity>
             </BoxShadow>
             <View style={{ width: 10 }} />
             <BoxShadow setting={shadowOpt} >
-                <TouchableOpacity style={{ height: buttonSize, width: buttonSize }} >
+                <TouchableOpacity style={{ height: buttonSize, width: buttonSize }} onPress={openOk}>
                     <OdnoklassnikiBrandsRound color="white" roundColor="rgb(226,94,18)"/>
                 </ TouchableOpacity>
             </BoxShadow>
             <View style={{ width: 10 }} />
             <BoxShadow setting={shadowOpt} >
-                <TouchableOpacity style={{ height: buttonSize, width: buttonSize }} >
+                <TouchableOpacity style={{ height: buttonSize, width: buttonSize }} onPress={openInstagram}>
                     <InstagramBrandsRound color="white" roundColor="rgb(226,94,18)"/>
                 </ TouchableOpacity>
             </BoxShadow>
